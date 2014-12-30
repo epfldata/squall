af9a7d879a0375efc52efe1ca3a08d994837436b
Mohammed ElSeidy <mohammed.elsaidy@epfl.ch>
2014-12-30 21:50:24 +0100

    Adding SquallTypes to switch between the imperative language types and the Scala types

---
+++ src/frontend/functional/scala/SquallType.scala
@@ -0,0 +1,50 @@
+package frontend.functional.scala
+
+import java.util.Date
+import java.text.SimpleDateFormat
+/**
+ * @author mohamed
+ */
+class SquallType {
+ 
+  trait SquallType[T] {
+  def convert(v: T): List[String]
+  def convertBack(v: List[String]): T
+  }
+
+  implicit def IntType = new SquallType[Int] {
+  def convert(v: Int): List[String] = List(v.toString)
+  def convertBack(v: List[String]): Int = v.head.toInt
+  }
+  
+  implicit def DoubleType = new SquallType[Double] {
+  def convert(v: Double): List[String] = List(v.toString)
+  def convertBack(v: List[String]): Double = v.head.toDouble
+  }
+
+  implicit def StringType = new SquallType[String] {
+  def convert(v: String): List[String] = List(v)
+  def convertBack(v: List[String]): String = v.head
+  }
+  
+  implicit def DateType = new SquallType[Date] {
+  def convert(v: Date): List[String] = List((new SimpleDateFormat("yyyy-MM-dd")).format(v))
+  def convertBack(v: List[String]): Date = (new SimpleDateFormat("yyyy-MM-dd")).parse(v.head)
+  }
+
+  implicit def tuple2Type[T1: SquallType, T2: SquallType] = new SquallType[Tuple2[T1, T2]]{
+  def convert(v: Tuple2[T1, T2]) = {
+     val st1 = implicitly[SquallType[T1]]
+     val st2 = implicitly[SquallType[T2]]
+     st1.convert(v._1) ++ st2.convert(v._2)
+  }
+  def convertBack(v: List[String]):Tuple2[T1, T2] = {
+     val st1 = implicitly[SquallType[T1]]
+     val st2 = implicitly[SquallType[T2]]
+     Tuple2(st1.convertBack(List(v(0))), st2.convertBack(List(v(1))))
+  }
+}
+
+  
+  
+}
--- src/frontend/functional/scala/Stream.scala
+++ src/frontend/functional/scala/Stream.scala
@@ -7,30 +7,30 @@ import scala.reflect.runtime.universe._
  */
 object Stream {
   
-  case class Source[T<:Product](name:String) extends Stream[T]
-  case class FilteredStream[T<:Product](Str:Stream[T], fn: T => Boolean) extends Stream[T]
-  case class MappedStream[T<:Product,U<:Product](Str:Stream[T], fn: T => U) extends Stream[U]
-  case class JoinedStream[T<:Product](Str1:Stream[T], Str2:Stream[T], ind: List[Int]) extends Stream[T]
-  case class GroupedStream[T<:Product,A](Str:Stream[T], agg: T => A, ind: List[Int]) extends TailStream[A]
+  case class Source[T<:SquallType](name:String) extends Stream[T]
+  case class FilteredStream[T<:SquallType](Str:Stream[T], fn: T => Boolean) extends Stream[T]
+  case class MappedStream[T<:SquallType,U<:SquallType](Str:Stream[T], fn: T => U) extends Stream[U]
+  case class JoinedStream[T<:SquallType](Str1:Stream[T], Str2:Stream[T], ind: List[Int]) extends Stream[T]
+  case class GroupedStream[T<:SquallType,A](Str:Stream[T], agg: T => A, ind: List[Int]) extends TailStream[T,A]
     
   //TODO change types to be generic
-   sealed trait Stream[T<:Product] {
+   sealed trait Stream[T<:SquallType] {
      def filter(fn: T => Boolean): Stream[T] = FilteredStream(this, fn)
-     def map[U<:Product](fn: T => U): Stream[U] = MappedStream[T,U](this, fn)
+     def map[U<:SquallType](fn: T => U): Stream[U] = MappedStream[T,U](this, fn)
      //def project(fn: List[Int]): Stream[List[String]] = map( myList =>  myList.zipWithIndex.filter(x=>myList.contains(x._2)).map(_._1) )
      def join(other: Stream[T], ind: List[Int]): Stream[T] = JoinedStream(this, other, ind)
-     def groupby[A](agg: T => A, ind: List[Int]): TailStream[A] = GroupedStream[T,A](this, agg, ind)
+     def groupby[A](agg: T => A, ind: List[Int]): TailStream[T,A] = GroupedStream[T,A](this, agg, ind)
    }
  
- sealed trait TailStream[A] extends Stream[List[String]]{
+   sealed trait TailStream[T,A] extends Stream[T]{
    
- }
+   }
  
 
- def createPlan[A](str:TailStream[A]){
-   interp(str)
+ def createPlan[T,A](str:TailStream[T,A]){
+   interp(str asInstanceOf Stream[T] )
  }
- def interp[T<:Product](str: Stream[T]): Unit = str match {
+ def interp[T<:SquallType](str: Stream[T]): Unit = str match {
   case Source(x) => {println("Reached Source")}
   case FilteredStream(parent, fn) => {interp(parent);println("Reached Filtered Stream")}
   case MappedStream(parent, fn) => {interp(parent);println("Reached Mapped Stream")}
@@ -40,7 +40,7 @@ object Stream {
  
  def main(args: Array[String]) {
    
-   createPlan(Source[Tuple1[Int]]("hello").filter{ x => true }.map{ y => Tuple1(2*y._1) }.groupby(x => x._1, List(1,2)));
+   //createPlan(Source[Tuple1[Int]]("hello").filter{ x => true }.map{ y => Tuple1(2*y._1) }.groupby(x => x._1, List(1,2)));
    
  }
  
