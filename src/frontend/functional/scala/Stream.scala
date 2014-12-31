d96a9677d63a01ae9745d5420c60fa1f355395bd
Mohammed ElSeidy <mohammed.elsaidy@epfl.ch>
2014-12-31 15:34:19 +0100

    Added the Scala DSL

--- src/frontend/functional/scala/Stream.scala
+++ src/frontend/functional/scala/Stream.scala
@@ -11,40 +11,51 @@ object Stream{
   case class Source[T:SquallType](name:String) extends Stream[T]
   case class FilteredStream[T:SquallType](Str:Stream[T], fn: T => Boolean) extends Stream[T]
   case class MappedStream[T:SquallType,U:SquallType](Str:Stream[T], fn: T => U) extends Stream[U]
-  case class JoinedStream[T:SquallType](Str1:Stream[T], Str2:Stream[T], ind: List[Int]) extends Stream[T]
+  case class JoinedStream[T:SquallType,U:SquallType,V:SquallType](Str1:Stream[T], Str2:Stream[U], ind: List[Int]) extends Stream[V]
   case class GroupedStream[T:SquallType,Number](Str:Stream[T], agg: T => Number, ind: List[Int]) extends TailStream[T,Number]
     
   //TODO change types to be generic
-   sealed trait Stream[T:SquallType]{
+   abstract class Stream[T:SquallType]{
+    val squalType: SquallType[T] = implicitly[SquallType[T]]
+    
      def filter(fn: T => Boolean): Stream[T] = FilteredStream(this, fn)
      def map[U:SquallType](fn: T => U): Stream[U] = MappedStream[T,U](this, fn)
-     //def project(fn: List[Int]): Stream[List[String]] = map( myList =>  myList.zipWithIndex.filter(x=>myList.contains(x._2)).map(_._1) )
-     def join(other: Stream[T], ind: List[Int]): Stream[T] = JoinedStream(this, other, ind)
+     def join[U:SquallType,V:SquallType](other: Stream[U], ind: List[Int]): Stream[V] = JoinedStream(this, other, ind)
      def groupby[Number](agg: T => Number, ind: List[Int]): TailStream[T,Number] = GroupedStream[T,Number](this, agg, ind)
+     
  }
  
-   sealed trait TailStream[T:SquallType,Number] extends Stream[T]{
-   
+   abstract class TailStream[T:SquallType,Number]{
+     
    }
  
 
- def createPlan[T: SquallType,Number](str:TailStream[T,Number]){
-   str match {
-     case s:Stream[T]=> interp(s) 
-   }
-   
+ def createPlan[T: SquallType,Number](str:TailStream[T,Number]): Unit = {
+   interp(str) 
  }
  def interp[T: SquallType](str: Stream[T]): Unit = str match {
   case Source(x) => {println("Reached Source")}
   case FilteredStream(parent, fn) => {interp(parent);println("Reached Filtered Stream")}
-  case MappedStream(parent, fn) => {interp(parent);println("Reached Mapped Stream")}
-  case JoinedStream(parent1, parent2, ind) => {interp(parent1);interp(parent2);println("Reached Joined Stream")}
+  case MappedStream(parent, fn) => {interp(parent)(parent.squalType);println("Reached Mapped Stream")}
+  case JoinedStream(parent1, parent2, ind) => {interp(parent1)(parent1.squalType);interp(parent2)(parent2.squalType);println("Reached Joined Stream")}      
+}
+ 
+  def interp[T: SquallType, Number](str:TailStream[T, Number]): Unit = str match {
   case GroupedStream(parent, agg, ind) => {interp(parent);println("Reached Grouped Stream")}      
 }
  
  def main(args: Array[String]) {
    
-   //createPlan(Source[Int]("hello").filter{ x:Int => true }.map[Int]{ y:Int => 2*y }.groupby(x => x._1, List(1,2)));
+   val x=Source[Int]("hello").filter{ x:Int => true }.map[(Int,Int)]{ y:Int => Tuple2(2*y,3*y) };
+   
+   val y = Source[Int]("hello").filter{ x:Int => true }.map[Int]{ y:Int => 2*y };
+   
+   val z = x.join[Int,(Int,Int)](y, List(2,3)).groupby(x => 3*x._2, List(1,2))
+   
+   interp(z)
+   
+   
+   
    
  }
  
