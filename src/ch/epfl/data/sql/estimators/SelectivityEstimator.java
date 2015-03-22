c6bdda108b6da65a7e8f0af9f6ca95f187d9bab2
Mohammed ElSeidy <mohammed.elsaidy@epfl.ch>
2015-03-22 21:38:40 +0100

    Removing unnecessary duplicate files.

--- src/ch/epfl/data/sql/estimators/SelectivityEstimator.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.data.sql.estimators;
-
-import java.beans.Expression;
-
-public interface SelectivityEstimator {
-
-	public double estimate(Expression expr);
-
-}
--- src/ch/epfl/data/sql/optimizers/Optimizer.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.data.sql.optimizers;
-
-import ch.epfl.data.plan_runner.query_plans.QueryBuilder;
-
-public interface Optimizer {
-
-	public QueryBuilder generate();
-
-}
