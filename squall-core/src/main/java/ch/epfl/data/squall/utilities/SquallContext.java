464a37ce208387a23155a95a098c67a63de39ed4
Daniel Espino Timón <akathorn@gmail.com>
2015-07-17 17:35:09 +0200

    Initial implementation of SquallContext

---
+++ squall-core/src/main/java/ch/epfl/data/squall/utilities/SquallContext.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2011-2015 EPFL DATA Laboratory
+ * Copyright (c) 2014-2015 The Squall Collaboration (see NOTICE)
+ *
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.epfl.data.squall.utilities;
+
+import ch.epfl.data.squall.query_plans.QueryBuilder;
+import ch.epfl.data.squall.storage.BasicStore;
+import ch.epfl.data.squall.utilities.StormWrapper;
+
+import org.apache.log4j.Logger;
+
+import backtype.storm.Config;
+
+/* This class represents a context of execution. It provides a unified
+ * interface for creating and submitting plans or running queries.
+ */
+public class SquallContext {
+  private static Logger LOG = Logger.getLogger(SquallContext.class);
+
+  private Config conf;
+  private boolean local;
+
+  public SquallContext() {
+    this(new Config());
+    // Load default values
+    SystemParameters.putInMap(conf, "DIP_EXTENSION", ".tbl");
+    SystemParameters.putInMap(conf, "DIP_READ_SPLIT_DELIMITER", "\\|");
+    SystemParameters.putInMap(conf, "DIP_GLOBAL_ADD_DELIMITER", "|");
+    SystemParameters.putInMap(conf, "DIP_GLOBAL_SPLIT_DELIMITER", "\\|");
+
+    SystemParameters.putInMap(conf, "DIP_KILL_AT_THE_END", "true");
+
+    SystemParameters.putInMap(conf, "STORAGE_LOCAL_DIR", "/tmp/ramdisk");
+    SystemParameters.putInMap(conf, "STORAGE_CLUSTER_DIR", "/data/squall_zone/storage");
+    SystemParameters.putInMap(conf, "STORAGE_COLD_START", "true");
+    SystemParameters.putInMap(conf, "STORAGE_MEMORY_SIZE_MB", "4096");
+
+    SystemParameters.putInMap(conf, "DIP_NUM_ACKERS", 0);
+
+    // TODO: load "local" from the configuration
+  }
+
+  public SquallContext(Config conf) {
+    this.conf = conf;
+  }
+
+  // Useful snippet from https://xkcd.com/221/
+  public int getRandomNumber() {
+    return 4; // chosen by fair dice roll.
+              // guaranteed to be random.
+  }
+
+  @Deprecated
+  public Config getConfiguration() {
+    return conf;
+  }
+
+  public void submit(String name, QueryBuilder plan) {
+    if (local) {
+      try {
+        submitLocal(name, plan);
+      } catch (InterruptedException e) {
+        LOG.warn(e.getStackTrace());
+      }
+    } else {
+      submitDistributed(name, plan);
+    }
+  }
+
+  public BasicStore<Object> submitLocal(String name, QueryBuilder plan) throws InterruptedException {
+    setLocal();
+
+    // TODO: name should be given in the plan somehow, as it is a property of
+    // the query
+    SystemParameters.putInMap(conf, "DIP_QUERY_NAME", name);
+    SystemParameters.putInMap(conf, "DIP_TOPOLOGY_NAME", name);
+
+    // TODO: use parallelisms that were already set
+    // TODO: take the parallelism from the component
+    setAllParallelisms(plan);
+
+    return StormWrapper.localSubmitAndWait(conf, plan);
+  }
+
+  public void submitDistributed(String name, QueryBuilder plan) {
+    setDistributed();
+
+    // TODO: name should be given in the plan somehow, as it is a property of
+    // the query
+
+    SystemParameters.putInMap(conf, "DIP_QUERY_NAME", name);
+    SystemParameters.putInMap(conf, "DIP_TOPOLOGY_NAME", name);
+
+    // TODO: use parallelisms that were already set
+    // TODO: take the parallelism from the component
+    setAllParallelisms(plan);
+
+    StormWrapper.submitTopology(conf, plan.createTopology(conf));
+  }
+
+  private void setAllParallelisms(QueryBuilder plan) {
+    for (String componentName: plan.getComponentNames()) {
+      SystemParameters.putInMap(conf, componentName + "_PAR", "1");
+    }
+  }
+
+  public void setLocal() {
+    SystemParameters.putInMap(conf, "DIP_DISTRIBUTED", "false");
+    SystemParameters.putInMap(conf, "DIP_DATA_PATH", "test/data/tpch/0.01G/");
+
+    local = true;
+  }
+
+  public void setDistributed() {
+    SystemParameters.putInMap(conf, "DIP_DISTRIBUTED", "true");
+    SystemParameters.putInMap(conf, "DIP_DATA_PATH", "/shared/tpch/0.01G/");
+
+    local = false;
+  }
+
+  public boolean isLocal() {
+    return local;
+  }
+
+  public boolean isDistributed() {
+    return !local;
+  }
+
+}
--- squall-functional/src/main/scala/ch/epfl/data/squall/api/scala/REPL.scala
+++ squall-functional/src/main/scala/ch/epfl/data/squall/api/scala/REPL.scala
@@ -24,12 +24,10 @@ import ch.epfl.data.squall.api.scala.SquallType._
 import ch.epfl.data.squall.api.scala.Stream._
 import ch.epfl.data.squall.api.scala.TPCHSchema._
 
+import ch.epfl.data.squall.utilities.SquallContext
+
 import scala.collection.JavaConversions._
 
-import ch.epfl.data.squall.utilities.StormWrapper
-import ch.epfl.data.squall.utilities.SystemParameters
-
-import backtype.storm.Config
 
 /** This class does not actually offer a REPL, but instead provides some
   * useful setup and methods to be imported when starting a REPL, for instance
@@ -40,34 +38,10 @@ import backtype.storm.Config
   *
   */
 class REPL(val outdir: String) {
-  // TODO: eventually most of this should be handled by a "context object"
-  // Initialize
-  val conf = new Config
-  var local = false
-
-  // Load default values
-  SystemParameters.putInMap(conf, "DIP_TOPOLOGY_NAME_PREFIX", "username")
-  SystemParameters.putInMap(conf, "DIP_EXTENSION", ".tbl")
-  SystemParameters.putInMap(conf, "DIP_READ_SPLIT_DELIMITER", "\\|")
-  SystemParameters.putInMap(conf, "DIP_GLOBAL_ADD_DELIMITER", "|")
-  SystemParameters.putInMap(conf, "DIP_GLOBAL_SPLIT_DELIMITER", "\\|")
-
-  SystemParameters.putInMap(conf, "DIP_KILL_AT_THE_END", "true")
-
-  SystemParameters.putInMap(conf, "STORAGE_LOCAL_DIR", "/tmp/ramdisk")
-  SystemParameters.putInMap(conf, "STORAGE_CLUSTER_DIR", "/data/squall_zone/storage")
-  SystemParameters.putInMap(conf, "STORAGE_COLD_START", "true")
-  SystemParameters.putInMap(conf, "STORAGE_MEMORY_SIZE_MB", "4096")
-
-  // Configure for tpch
-  SystemParameters.putInMap(conf, "CUSTOMER_PAR", "1")
-  SystemParameters.putInMap(conf, "ORDERS_PAR", "1")
-  SystemParameters.putInMap(conf, "LINEITEM_PAR", "1")
-  SystemParameters.putInMap(conf, "CUSTOMER_ORDERS_PAR", "1")
-  SystemParameters.putInMap(conf, "CUSTOMER_ORDERS_LINEITEM_PAR", "1")
+  val context = new SquallContext
 
   def start() = {
-print("""
+    print("""
  ____   ___  _   _   _    _     _
 / ___| / _ \| | | | / \  | |   | |
 \___ \| | | | | | |/ _ \ | |   | |
@@ -77,7 +51,7 @@ print("""
 Type "help" for Squall related help
 
 """)
-    setLocal
+    context.setLocal
   }
 
   // TODO: make a more useful help. Maybe use a Map to define the possible
@@ -89,11 +63,11 @@ Type "help" for Squall related help
   private def packClasses(): String = {
     println("Packing jar file...")
     import scala.sys.process._
-    if(local) {
+    if(context.isLocal()) {
       (s"jar cf ${outdir}/repl.jar -C ${outdir}/classes/ .").!!
     } else {
       (s"cp squall-functional/target/squall-frontend-standalone-0.2.0.jar ${outdir}/repl.jar").!!
-      (s"jar uf ${outdir}/repl.jar -C ${outdir}/classes/ .").!!
+        (s"jar uf ${outdir}/repl.jar -C ${outdir}/classes/ .").!!
     }
     println("Done packing")
     s"${outdir}/repl.jar"
@@ -125,42 +99,23 @@ Type "help" for Squall related help
     // Configure the query. To easily identify it we use the prefixes repl_0_,
     // repl_1_, repl_2_... Followed by a random number to avoid exceptions
     // telling us that the topology already exists.
-    val tpname = "repl_" + count + "_" + scala.util.Random.nextInt()
-    SystemParameters.putInMap(conf, "DIP_QUERY_NAME", "repl_" + count)
-    SystemParameters.putInMap(conf, "DIP_TOPOLOGY_NAME", tpname)
     count = count + 1
-
-    tpname
+    "repl_" + count + "_" + scala.util.Random.nextInt()
   }
 
-  def submit(queryPlan: QueryBuilder) = {
+
+
+  // If submitting locally we can actually get a result by calling submitLocal
+  def submitLocal(plan: QueryBuilder) = {
     val tpname = prepareSubmit()
-
-    // Create and send the topology
-    if (local) {
-      StormWrapper.localSubmitAndWait(conf, queryPlan)
-    } else {
-      StormWrapper.submitTopology(conf, queryPlan.createTopology(conf))
-      println("Submitted topology as " + tpname)
-    }
+    context.submitLocal(tpname, plan)
   }
 
-  def setDistributed() = {
-    SystemParameters.putInMap(conf, "DIP_DISTRIBUTED", "true")
-    SystemParameters.putInMap(conf, "DIP_DATA_PATH", "/shared/tpch/0.01G/")
-
-    local = false
-    println("Mode set to local")
+  def submitDistributed(plan: QueryBuilder) = {
+    val tpname = prepareSubmit()
+    context.submit(tpname, plan)
   }
 
-  def setLocal() = {
-    SystemParameters.putInMap(conf, "DIP_DATA_PATH", "test/data/tpch/0.01G/")
-    SystemParameters.putInMap(conf, "DIP_DISTRIBUTED", "false")
-    SystemParameters.putInMap(conf, "DIP_NUM_ACKERS", 0)
-
-    local = true
-    println("Mode set to local")
-  }
 
   // An example query plan
   def createQueryPlan(): QueryBuilder = {
@@ -168,7 +123,7 @@ Type "help" for Squall related help
     val orders = Source[Orders]("orders").map( _.custkey )
     val join = (customers join orders)( _._1 )( x => x )
     val agg = join.groupByKey( x => 1, _._1._2)
-    val plan = agg.execute(conf)
+    val plan = agg.execute(context.getConfiguration())
     plan
   }
 
