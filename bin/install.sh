76a0cdbc228c8bec7d7e0d312c8be2bca4635b1e
khgl <khayyam.guliyev@epfl.ch>
2016-03-28 12:28:58 +0200

    Merger with Master.

---
+++ bin/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. ./storm_env.sh
+
+# On Linux machines, we need to install Scala (at least 2.10)
+# wget http://www.scala-lang.org/files/archive/scala-2.10.4.deb
+# sudo dpkg -i scala-2.10.4.deb
+# sudo apt-get update
+# sudo apt-get install scala
+
+# Downloading Storm and putting it to the right place
+echo "Downloading and extracting $STORMNAME ..."
+wget http://mirror.easyname.ch/apache/storm/$STORMNAME/$STORMNAME.tar.gz
+tar -xzf $STORMNAME.tar.gz
+mv $STORMNAME ..
+rm $STORMNAME.tar.gz
+
+# Compiling Squall and generating dependencies 
+echo "Compiling Squall and generating dependencies ..."
+CURR_DIR=`pwd`
+cd ..
+sbt package
+sbt assemblyPackageDependency
+cd $CURR_DIR
+# The following is used only for the Cluster Mode
+cp ../squall-core/target/squall-dependencies-0.2.0.jar ../$STORMNAME/lib/
+
+# Installation of dbtoaster
+./install_dbtoaster.sh
---
+++ bin/squall_local.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+. ./storm_env.sh
+
+printFormat (){
+	echo "Format:     ./squall_local.sh MODE CONFIG_PATH"
+	echo "             MODE: SQL (default) or PLAN_RUNNER"
+}
+
+# Check the number of arguments
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+	echo "ERROR:: Improper number of arguments!"
+	printFormat
+	exit 1
+fi
+
+# Reading input paramters
+MODE=$1
+if [[ "$MODE" != "PLAN_RUNNER" && "$MODE" != "SQL" ]]; then
+	MODE=SQL	
+else
+   shift
+fi
+
+# Main class
+if [ "$MODE" == "PLAN_RUNNER" ]; then
+	MAIN_CLASS=ch.epfl.data.squall.main.Main
+else
+	MAIN_CLASS=ch.epfl.data.squall.api.sql.main.ParserMain
+fi
+
+# Set config file path and check if it exist
+if [[ $# -lt 1 ]]; then
+	echo "ERROR:: Missing configuration file path!"
+	printFormat
+	exit 1
+fi
+CONFIG_PATH=$1
+if ! [ -f $CONFIG_PATH ]; then
+	echo "ERROR:: File $CONFIG_PATH does not exist! Please specify a valid configuration file!"
+	exit 1
+fi
+
+# Running
+java -Xmx128m -cp ../squall-core/target/squall-0.2.0.jar:../squall-core/target/squall-dependencies-0.2.0.jar:../$STORMNAME/lib/* $MAIN_CLASS $CONFIG_PATH
